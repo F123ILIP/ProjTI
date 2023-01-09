090c4e2a7d36124be7c37d2431089d9a7cba0d6b
maciegg5 <maciekkrakow2012@gmail.com>
2023-01-09 17:58:41 +0100

    menu + zegar + background

---
+++ script.js
@@ -0,0 +1,20 @@
+function zegar(){
+
+var dzisiaj = new Date();
+
+var dzien=dzisiaj.getDate();
+var miesiac=dzisiaj.getMonth()+1;
+var rok=dzisiaj.getFullYear();
+
+var godzina = dzisiaj.getHours();
+    if(godzina<10) godzina="0"+godzina;
+    var minuta = dzisiaj.getMinutes();
+    if(minuta<10) minuta="0"+minuta;
+    var sekunda = dzisiaj.getSeconds();
+    if(sekunda<10) sekunda="0"+sekunda;
+
+    document.getElementById("zegar").innerHTML = godzina+":"+minuta+":"+sekunda;
+
+    setTimeout("zegar()",1000);
+
+}
