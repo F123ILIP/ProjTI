7a6f32aa0eb77e73c2aba92428be937cf6f28e61
F123ILIP <122181564+F123ILIP@users.noreply.github.com>
2023-01-13 17:12:01 +0100

    sklep

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ var panel = document.getElementById("#logowanie");
 panel = location.href = "login.html";
 
 }
+
+/*=================================*/ /*SEARCH*/ /*=================================*/
+
+function search() 
+{
+    let input = document.getElementById("searchbar").value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName("Zdjecie");
+      
+    for( i = 0; i < x.length; i++ )
+		{ 
+          if( !x[i].innerHTML.toLowerCase().includes( input ) ) x[i].style.display="none";
+          else x[i].style.display="list-item";                 
+		}
+}
